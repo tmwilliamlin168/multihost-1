33cd8b6771602624f79eef757eff5759832ac378
tmwilliamlin168 <8696482+tmwilliamlin168@users.noreply.github.com>
2022-08-02 18:35:40 +0800

    dotenv & port env

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
+import dotenv from 'dotenv';
 import express from 'express';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
