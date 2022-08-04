6ece060008cd5c40a7a3832405e513831f7be0d9
tmwilliamlin168 <8696482+tmwilliamlin168@users.noreply.github.com>
2022-08-04 21:42:21 +0800

    vhost

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import vhost from 'vhost';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(
+  vhost(
+    `*.${process.env.HOST}`,
+    express().get('/', (req, res) => res.send(`Your host is ${req.hostname}`)),
+  ),
+);
+app.get('/', (req, res) => res.send('Root / default'));
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
